478e7fdfbb6cf282b017156050893ee229f33678
harinarayan14 <gjayashankar2008@gmail.com>
2020-04-17 17:11:38 +0530

    completed the class

--- Pig.js
+++ Pig.js
@@ -2,6 +2,20 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.v  =255;
   }
+display(){
+if(this.body.speed<3){
+  super.display();
+}
+else{
+  push();
+  this.v-=5;
+  tint(255,this.v);
+  image(this.image,this.body.position.x,this.body.position.y,50,50);
+  pop();
+  World.remove(world,this.body);
+}
 
+}
 };
